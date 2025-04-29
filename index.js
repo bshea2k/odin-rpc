b7595a6d7a5474c91087b788b393d1dc4f994ca0
Bryant Shea <bshea2k@gmail.com>
2025-04-28 17:14:07 -0700

    Implement function to get user input

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
+
     return options[choice];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please type in a number to represent your choice, rock [1], paper [2], or scissors [3].");
+
+    return options[choice - 1];
 }
