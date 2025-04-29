65bd58f19332a98493eeb6a3930f060528c15beb
Bryant Shea <bshea2k@gmail.com>
2025-04-28 17:45:49 -0700

    Implement function to simulate a round

--- index.js
+++ index.js
@@ -9,7 +9,37 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Please type in a number to represent your choice, rock [1], paper [2], or scissors [3].");
+    let choice = parseInt(prompt("Please type in a number to represent your choice, rock [1], paper [2], or scissors [3]."));
 
     return options[choice - 1];
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie");
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("Loss. Computer chose paper.");
+            computerScore++;
+        } else {
+            console.log("Win. Computer chose scissors.");
+            humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("Win. Computer chose rock.");
+            humanScore++;
+        } else {
+            console.log("Loss. Computer chose scissors.");
+            computerScore++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("Loss. Computer chose rock.");
+            computerScore++;
+        } else {
+            console.log("Win. Computer chose paper.");
+            humanScore++;
+        }
+    }
 }
