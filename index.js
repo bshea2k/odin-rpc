8a62f865b9927c42fb137a34605fc384ae11b8f6
Bryant Shea <bshea2k@gmail.com>
2025-04-28 17:49:59 -0700

    Implement function to play a game of 5 rounds

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const options = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -41,5 +39,14 @@ function playRound(humanChoice, computerChoice) {
             console.log("Win. Computer chose paper.");
             humanScore++;
         }
+    }
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getcomputerChoice());
     }
 }
